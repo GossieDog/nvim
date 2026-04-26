9e00a351a84ed8195bc9b333071510b76ddb25a6
GossieDog <erickylefan@gmail.com>
2026-04-25 17:04:15 -0700

    Explicitly disable autoload.

--- lua/plugins/session-manager.lua
+++ lua/plugins/session-manager.lua
@@ -4,4 +4,10 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
+	config = function()
+		local config = require("session_manager.config")
+		require("session_manager").setup({
+			autoload_mode = config.AutoloadMode.Disabled,
+		})
+	end,
 }
