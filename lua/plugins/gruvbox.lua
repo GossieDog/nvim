18295f31fa7a4938121fe197cc90db462657aa0e
Eric Fan <erickylefan@gmail.com>
2026-05-20 10:19:41 -0600

    Add and comment out bufferline fill

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -10,6 +10,7 @@ return {
 			vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { bg = "#282828" })
 			vim.api.nvim_set_hl(0, "BlinkCmpDoc", { bg = "#282828" })
 			vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", { bg = "#282828" })
+			-- vim.api.nvim_set_hl(0, "BufferLineFill", { bg = "#282828" })
 		end,
 	},
 }
