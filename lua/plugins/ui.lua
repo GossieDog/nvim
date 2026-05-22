b020b56ac312a80761ec2a1ce90952500d978931
Eric Fan <erickylefan@gmail.com>
2026-05-22 15:11:12 -0600

    Clean up nvimtree config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -140,32 +140,33 @@ return {
 			-- optionally enable 24-bit colour
 			vim.opt.termguicolors = true
 		end,
-		---@type nvim_tree.config
-		opts = {
-			git = {
-				enable = true,
-				ignore = true,
-				timeout = 500,
-			},
-			sort = {
-				sorter = "case_sensitive",
-			},
-			view = {
-				width = 30,
-			},
-			renderer = {
-				group_empty = true,
-			},
-			filters = {
-				dotfiles = true,
-			},
-			sync_root_with_cwd = true,
-			respect_buf_cwd = true,
-			update_focused_file = {
-				enable = true,
-				update_cwd = true,
-			},
-		},
+		config = function()
+			require("nvim-tree").setup({
+				git = {
+					enable = true,
+					ignore = true,
+					timeout = 500,
+				},
+				sort = {
+					sorter = "case_sensitive",
+				},
+				view = {
+					width = 30,
+				},
+				renderer = {
+					group_empty = true,
+				},
+				filters = {
+					dotfiles = true,
+				},
+				sync_root_with_cwd = true,
+				respect_buf_cwd = true,
+				update_focused_file = {
+					enable = true,
+					update_cwd = true,
+				},
+			})
+		end,
 	},
 	{
 		"folke/snacks.nvim",
