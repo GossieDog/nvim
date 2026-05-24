551157bc3f84b4c11709729ba42f95f7071cdf94
Eric Fan <erickylefan@gmail.com>
2026-05-24 09:02:07 -0600

    Match lualine diagnostic icons to signcolum diagnostic icons

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -45,7 +45,18 @@ return {
 				},
 				sections = {
 					lualine_a = { "mode" },
-					lualine_b = { "branch", "diagnostics" },
+					lualine_b = {
+						"branch",
+						{
+							"diagnostics",
+							symbols = {
+								error = " ",
+								warn = " ",
+								info = " ",
+								hint = " ",
+							},
+						},
+					},
 					lualine_c = { { "filename", path = 1 } },
 					lualine_x = { "encoding", "filetype", "diff" },
 					lualine_y = { "progress" },
