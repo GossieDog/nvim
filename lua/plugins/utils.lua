00880962492db1ff0727986d6b4f328b9f517efb
Eric Fan <erickylefan@gmail.com>
2026-05-27 13:00:22 -0600

    Change luasnip to lazy load

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -252,7 +252,7 @@ return {
 	},
 	{
 		"L3MON4D3/LuaSnip",
-		event = { "InsertEnter", "CmdlineEnter" },
+		lazy = true,
 		dependencies = { "rafamadriz/friendly-snippets" },
 		-- follow latest release.
 		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
