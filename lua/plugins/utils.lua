3ae7829fcffd324dbb0b38fb602e4385f93e2cec
Eric Fan <erickylefan@gmail.com>
2026-05-27 10:41:08 -0600

    lazy load cloak and luasnip

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -252,6 +252,7 @@ return {
 	},
 	{
 		"L3MON4D3/LuaSnip",
+		event = { "InsertEnter", "CmdlineEnter" },
 		dependencies = { "rafamadriz/friendly-snippets" },
 		-- follow latest release.
 		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
