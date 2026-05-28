ece22318cff43770bfb4747513b7d311427e70db
Eric Fan <erickylefan@gmail.com>
2026-05-28 08:06:58 -0600

    Set global status to true for lualine.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -27,6 +27,7 @@ return {
 					ignore_focus = {},
 					always_divide_middle = true,
 					always_show_tabline = true,
+					globalstatus = true,
 					refresh = {
 						statusline = 1000,
 						-- tabline = 1000,
