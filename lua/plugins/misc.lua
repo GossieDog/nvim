2c013ddbee3804ea8f0ae36f68c386c289f55709
Eric Fan <erickylefan@gmail.com>
2026-05-29 08:56:49 -0600

    Add html, css, and javascript linters.

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -17,7 +17,17 @@ return {
 		config = function()
 			require("lint").linters_by_ft = {
 				tex = { "chktex" },
+				html = { "htmlhint" },
+				css = { "stylelint" },
+				javascript = { "eslint_d" },
 			}
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					-- try_lint without arguments runs the linters defined in `linters_by_ft`
+					-- for the current filetype
+					require("lint").try_lint()
+				end,
+			})
 		end,
 	},
 	{
