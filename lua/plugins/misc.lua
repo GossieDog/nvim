282ccd6552f9b7524267c0472db932e4d7b62228
Eric Fan <erickylefan@gmail.com>
2026-05-31 15:54:06 -0600

    Improve lint autocommand

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -21,7 +21,11 @@ return {
 				css = { "stylelint" },
 				javascript = { "eslint_d" },
 			}
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
 				callback = function()
 					-- try_lint without arguments runs the linters defined in `linters_by_ft`
 					-- for the current filetype
