5fdb9c7a01c8aecd31b703546bb095bf5d0a758d
Eric Fan <erickylefan@gmail.com>
2026-06-07 07:56:28 -0600

    Set virtual diagnostic float colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,12 +4,12 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd([[colorscheme gruvbox]])
-			-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 			vim.api.nvim_set_hl(0, "BlinkCmpMenu", { bg = "#282828" })
 			vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { bg = "#282828" })
 			vim.api.nvim_set_hl(0, "BlinkCmpDoc", { bg = "#282828" })
 			vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", { bg = "#282828" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#282828" })
+			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#282828" })
 			-- vim.api.nvim_set_hl(0, "BufferLineFill", { bg = "#282828" })
 		end,
 	},
