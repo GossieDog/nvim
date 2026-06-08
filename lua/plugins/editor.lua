b4e195f866c0fea3f07aa9360b5ce75e27c95fbb
Eric Fan <erickylefan@gmail.com>
2026-06-08 15:40:59 -0600

    Enable vimtex folding and remove sioyek line

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -60,7 +60,7 @@ return {
 			vim.g.vimtex_view_method = "zathura"
 			vim.g.vimtex_view_general_viewer = "zathura"
 			vim.g.vimtex_context_pdf_viewer = "zathura"
-			-- vim.g.vimtex_view_sioyek_options = "--new-window"
+			vim.g.vimtex_fold_enabled = 1
 			vim.g.vimtex_bibliography_file_handler = function(file)
 				vim.fn.jobstart({ "open", file }, { detach = true })
 			end
