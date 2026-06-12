c56135df910d2ff82df743861089aab7ee311b69
Eric Fan <erickylefan@gmail.com>
2026-06-12 10:42:01 -0600

    Cleanup vimtex config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -61,82 +61,44 @@ return {
 			vim.g.vimtex_view_general_viewer = "zathura"
 			vim.g.vimtex_context_pdf_viewer = "zathura"
 
+			vim.g.vimtex_quickfix_mode = 0
+
 			vim.g.vimtex_fold_enabled = 1
 			vim.g.vimtex_fold_types = {
 				envs = { enabled = 0 },
 				items = { enabled = 0 },
 			}
-
-			vim.g.vimtex_bibliography_file_handler = function(file)
-				vim.fn.jobstart({ "open", file }, { detach = true })
-			end
-			vim.g.vimtex_quickfix_ignore_filters = { -- Filter out common noise
+			vim.g.vimtex_quickfix_ignore_filters = {
 				"Underfull",
 				"Overfull",
-				"specifier changed to",
-				"Token not allowed in a PDF string",
-				"Package hyperref Warning",
 				"Marginpar on page",
-			}
-			vim.g.vimtex_log_ignore = { -- Suppress specific log messages
-				"Underfull",
-				"Overfull",
-				"specifier changed to",
-				"Token not allowed in a PDF string",
-				"Package hyperref Warning",
-				"Marginpar on page",
-			}
-			vim.g.vimtex_clean_enabled = 1
-			vim.g.vimtex_clean_patterns = {
-				"*.aux",
-				"*.bbl",
-				"*.blg",
-				"*.brf",
-				"*.fls",
-				"*.fdb_latexmk",
-				"*.log",
-				"*.out",
-				"*.toc",
-				"*.nav",
-				"*.snm",
-				"*.vrb",
-				"*.run.xml",
-				"*.bcf",
-				"*.synctex",
-				"*.synctex.gz",
 			}
 		end,
 		config = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "tex",
-				callback = function()
-					vim.opt_local.spell = true
-					vim.opt_local.spelllang = { "en_us" }
-					vim.keymap.set(
-						"n",
-						"<localleader>lw",
-						"<cmd>VimtexCountWords!<cr>",
-						{ buffer = true, desc = "VimTeX Count Words" }
-					)
-					vim.keymap.set("n", "<localleader>lt", function()
-						return require("vimtex.snacks").toc()
-					end, { buffer = true, desc = "Open TOC", remap = false })
-				end,
-			})
-			-- vim.api.nvim_create_autocmd("VimLeavePre", {
-			-- 	pattern = "*.tex",
+			-- vim.api.nvim_create_autocmd("FileType", {
+			-- 	pattern = "tex",
 			-- 	callback = function()
-			-- 		vim.cmd("VimtexClean")
+			-- 		vim.opt_local.spell = true
+			-- 		vim.opt_local.spelllang = { "en_us" }
+			-- 		vim.keymap.set(
+			-- 			"n",
+			-- 			"<localleader>lw",
+			-- 			"<cmd>VimtexCountWords!<cr>",
+			-- 			{ buffer = true, desc = "VimTeX Count Words" }
+			-- 		)
+			-- 		vim.keymap.set("n", "<localleader>lt", function()
+			-- 			return require("vimtex.snacks").toc()
+			-- 		end, { buffer = true, desc = "Open TOC", remap = false })
 			-- 	end,
 			-- })
-			vim.api.nvim_create_user_command("TexCountSection", function()
-				local output = vim.fn.systemlist("texcount -sub=section " .. vim.fn.expand("%"))
-				vim.cmd("new")
-				vim.bo.buftype = "nofile"
-				vim.bo.bufhidden = "wipe"
-				vim.bo.swapfile = false
-				vim.api.nvim_buf_set_lines(0, 0, -1, false, output)
-			end, {})
+			-- vim.api.nvim_create_user_command("TexCountSection", function()
+			-- 	local output = vim.fn.systemlist("texcount -sub=section " .. vim.fn.expand("%"))
+			-- 	vim.cmd("new")
+			-- 	vim.bo.buftype = "nofile"
+			-- 	vim.bo.bufhidden = "wipe"
+			-- 	vim.bo.swapfile = false
+			-- 	vim.api.nvim_buf_set_lines(0, 0, -1, false, output)
+			-- end, {})
 		end,
 	},
 	{
