c75367de8216aac1989c1e8d651b835e1222febd
Eric Fan <erickylefan@gmail.com>
2026-07-16 22:14:54 -0600

    Remove cloak from practice-journal

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -67,7 +67,7 @@ return {
 					{
 						-- Match any file starting with '.env'.
 						-- This can be a table to match multiple file patterns.
-						file_pattern = "Practice-Journal.tex",
+						file_pattern = "Practice-Diary.tex",
 						-- Match an equals sign and any character after it.
 						-- This can also be a table of patterns to cloak,
 						-- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
@@ -76,11 +76,6 @@ return {
 						-- The actual replacement will contain the 'cloak_character'
 						-- where it doesn't cover the original text.
 						-- If left empty the legacy behavior of keeping the first character is retained.
-						replace = nil,
-					},
-					{
-						file_pattern = "Practice-Diary.tex",
-						cloak_pattern = ".+",
 					},
 					{
 						file_pattern = ".zshrc",
