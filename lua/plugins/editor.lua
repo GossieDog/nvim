adeb08e3d25d6108748b707cd25184830bcf34cd
Eric Fan <erickylefan@gmail.com>
2026-08-18 09:03:27 -0600

    Clean up flash config and add snacks_notif to flash exclude list

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -4,30 +4,62 @@ return {
 		event = "VeryLazy",
 		---@type Flash.Config
 		opts = {
-			modes = {
-				char = {
-					enabled = false, -- Disable char mode so 'f' uses jump() instead
+			search = {
+				exclude = {
+					"notify",
+					"snacks_notif",
+					"cmp_menu",
+					"noice",
+					"flash_prompt",
+					function(win)
+						-- exclude non-focusable windows
+						return not vim.api.nvim_win_get_config(win).focusable
+					end,
 				},
 			},
 		},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-    -- Simulate nvim-treesitter incremental selection
-    { "<c-space>", mode = { "n", "o", "x" },
-      function()
-        require("flash").treesitter({
-          actions = {
-            ["<c-space>"] = "next",
-            ["<BS>"] = "prev"
-          }
-        })
-      end, desc = "Treesitter Incremental Selection" },
-  },
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
 	},
 	{
 		"stevearc/conform.nvim",
