b078c467e23029d5eb4fb7a7cef1f9e69e107b9f
Eric Fan <erickylefan@gmail.com>
2026-09-21 10:07:06 -0700

    Remove extra yanky dependency

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -212,9 +212,6 @@ return {
 	},
 	{
 		"gbprod/yanky.nvim",
-		dependencies = {
-			{ "nvim-telescope/telescope.nvim", lazy = true },
-		},
 		opts = {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
